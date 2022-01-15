c0abf1f1e1ce0cf4c946837464ccbd39b2d38cd0
unknown <satvikbatra@gmail.com>
2022-01-15 13:37:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { useState,useRef,useCallback } from 'react';
 import './App.css';
-
+import useSearch from './Search'
 function App() {
+  
+  /* we can use the custom search hook now. */
+
+  const [query, setQuery] = useState(''); 
+  const [pagenum, setPage] = useState(1);
+
+  useSearch(query, pagenum);
+  const { books, hasMore, loading, error } = useSearch(query,pagenum); 
+  
+  const obeserver = useRef();
+  
+  /* now ref mantains the value inside it even if the page is re rendered , ref value changing does not cause a re render. */
+  
+  const lastbookElementRef = useCallback(node => {
+    if (loading) return;
+    if (obeserver.current) obeserver.current.disconnect();
+    obeserver.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPage(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) obeserver.current.observe(node); 
+  },[loading,hasMore])
+
+
+  function handleChange(e) {
+    /* so everytime we change the state , the component is re rendered 
+       & the useSearch hook is called.
+    */
+    
+    setQuery(e.target.value);
+    setPage(1);
+  }
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Infinite Scroll Assignment</h1>
+      <h3> - using the open library API</h3>
+      <input type="text" value={query}  onChange={handleChange}></input>
+      <ul>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <li key={book} ref={lastbookElementRef} >{book}</li>
+        } else {
+          return <li key={book} >{book}</li>
+        }
+      })}
+      </ul>
+
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error...'}</div>
     </div>
   );
 }
 
 export default App;
+
+/* use Ref most important property is that it does not re render every time its
+   value is changed  , unlike the state hook which causes a re render everytime,
+   therefore storing previous values or counting the number of renders , useRef
+*/

2c35b4e3fa04b904431d8ca2d3a06c84931546ad
unknown <satvikbatra@gmail.com>
2022-01-16 17:04:28 +0530

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,57 +1,94 @@
-import { useState,useRef,useCallback } from 'react';
+import { useState,useRef,useCallback,useEffect} from 'react';
 import './App.css';
-import useSearch from './Search'
+import axios from 'axios';
+
 function App() {
   
   /* we can use the custom search hook now. */
 
-  const [query, setQuery] = useState(''); 
-  const [pagenum, setPage] = useState(1);
-
-  useSearch(query, pagenum);
-  const { books, hasMore, loading, error } = useSearch(query,pagenum); 
-  
-  const obeserver = useRef();
-  
-  /* now ref mantains the value inside it even if the page is re rendered , ref value changing does not cause a re render. */
-  
-  const lastbookElementRef = useCallback(node => {
-    if (loading) return;
-    if (obeserver.current) obeserver.current.disconnect();
-    obeserver.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMore) {
-        setPage(prevPageNumber => prevPageNumber + 1)
-      }
-    })
-    if (node) obeserver.current.observe(node); 
-  },[loading,hasMore])
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setErrors] = useState(false);
+  const [results, setResults] = useState([]);
 
 
-  function handleChange(e) {
-    /* so everytime we change the state , the component is re rendered 
-       & the useSearch hook is called.
-    */
-    
-    setQuery(e.target.value);
-    setPage(1);
+  useEffect(() => {
+    myFunction();
+    return () => {
+      setResults([]); 
+    };
+  }, []);
+  
+  function myFunction() {
+    setResults(
+      [[{
+        name: 'apple',
+        _id: 'ABC10982'
+      }]]
+    );
   }
   
+  /* var results = [
+    {
+     name: 'apple',
+    _id: 'ABC10982'
+    }
+  ] */
+
+  /* useEffect(() => {
+    console.log('refreshed');
+  }, [results]); 
+ */
+  function handleChange(e) {
+    setResults([]);
+   /*  results = []; */
+    setLoading(true);
+    axios.get(`http://localhost:5000/search`, {
+        params: {
+          querystring: e.target.value 
+        }
+      }).then((res) => {
+        
+       /*  for (var i = 0; i < res.data.data.length; i++){
+         
+          console.log(res.data.data[i].item["name"]);
+           results.push({
+            _id:res.data.data[i].item["_id"],
+            name:res.data.data[i].item["name"]
+          }); 
+        } */
+        console.log(res.data.data);
+        setResults((prev) => {
+          return [...prev, ...res.data.data]
+        });
+        console.log(results);
+        setLoading(false);
+        setErrors(false);
+      }, (e) => {
+        console.log(e);
+        setErrors(true);
+    });
 
 
+  }
   return (
     <div className="App">
-      <h1> Infinite Scroll Assignment</h1>
-      <h3> - using the open library API</h3>
-      <input type="text" value={query}  onChange={handleChange}></input>
+      <h1> Search Assignment</h1>
+      <h3> - using Node.js API & MongoDB</h3>
+      <input type="text" onChange={(e) =>
+      { 
+       
+        setQuery(e.target.value);
+        handleChange(e);
+      }
+      }></input>
       <ul>
-      {books.map((book, index) => {
-        if (books.length === index + 1) {
-          return <li key={book} ref={lastbookElementRef} >{book}</li>
-        } else {
-          return <li key={book} >{book}</li>
-        }
+      { results.map((result) => {
+         return <li key={result._id}>{result.name}</li>
       })}
       </ul>
+
+      {results.length===0 && query!="" && <h4>No Results Found :( </h4>}
 
       <div>{loading && 'Loading...'}</div>
       <div>{error && 'Error...'}</div>
